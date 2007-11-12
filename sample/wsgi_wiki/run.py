d9a7050bd675ef37c4b233d42479d7b6846cf4d9
william <william@opensource4you.com>
2007-11-12 19:48:50 +0100

    improvement on the wsgi_wiki sample

--- sample/wsgi_wiki/run.py
+++ sample/wsgi_wiki/run.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 
 import _evhttp as evhttp
-from fapws2 import base
+import fapws2 
+import fapws2.base
+import fapws2.contrib
+import fapws2.contrib.views
 import sys
 sys.setcheckinterval=100000 # since we don't use threads, internal checks are no more required
 
 import views
 
+
 def start():
     evhttp.start("0.0.0.0", 8080)
     
-    evhttp.set_base_module(base)
+    evhttp.set_base_module(fapws2.base)
     
     def generic(environ, start_response):
         return ["page not found"]
@@ -23,18 +27,13 @@ def start():
     def edit(environ, start_response):
         r=views.Edit()
         return r(environ, start_response)
-    
-    def staticfile(environ, start_response):
-        try:
-            f=open("static/"+ environ['PATH_INFO'], "rb")
-        except:
-            f=["Page not found"]
-        return f
     def favicon(environ, start_response):
         return open("static/img/favicon.ico", "rb")
     
     evhttp.http_cb("/display/",display)
     evhttp.http_cb("/edit", edit)
+    evhttp.http_cb("/new", edit)
+    staticfile=fapws2.contrib.views.Staticfile("static/")
     evhttp.http_cb("/static/",staticfile)
     evhttp.http_cb('/favicon.ico',favicon),
 
