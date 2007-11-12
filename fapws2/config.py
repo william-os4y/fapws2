fbb9137f486ecb3c22ababae31c33319dfcb379c
william <william@opensource4you.com>
2007-11-12 15:23:56 +0100

    got for fapws2-0.2

--- fapws2/config.py
+++ fapws2/config.py
@@ -1,4 +1,4 @@
-SERVER_IDENT = "fapws2/0.1"
+SERVER_IDENT = "fapws2/0.2"
 send_traceback_to_browser=True
 #in case of False for send_traceback_to_browser, send_traceback_short will be sent to the browser
 send_traceback_short="Please contact your administrator"
