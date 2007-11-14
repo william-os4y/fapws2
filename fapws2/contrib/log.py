9629ccc6ea975fea8a316c0a5e554a4f171c7b2d
william <william@opensource4you.com>
2007-11-14 20:06:54 +0100

    fix on the date format of log.py

--- fapws2/contrib/log.py
+++ fapws2/contrib/log.py
@@ -18,7 +18,7 @@ class Log:
                 size=os.path.getsize(res.name)
             #this is provided by a proxy or direct
             remote_host=environ.get('HTTP_X_FORWARDED_FOR',environ['fapws.remote_host'])
-            self.output.write("%s %s - [%s] \"%s %s %s/%s.%s\" %s %s \"%s\" \"%s\"\n" % (remote_host, environ['HTTP_HOST'], tts, environ['REQUEST_METHOD'], environ['fapws.uri'], environ['wsgi.url_scheme'], environ['fapws.http_major'], environ['fapws.http_minor'], start_response.status_code, size, environ.get("HTTP_REFERER", "-"), environ['HTTP_USER_AGENT']))
+            self.output.write("%s %s - [%s GMT] \"%s %s %s/%s.%s\" %s %s \"%s\" \"%s\"\n" % (remote_host, environ['HTTP_HOST'], tts, environ['REQUEST_METHOD'], environ['fapws.uri'], environ['wsgi.url_scheme'], environ['fapws.http_major'], environ['fapws.http_minor'], start_response.status_code, size, environ.get("HTTP_REFERER", "-"), environ['HTTP_USER_AGENT']))
             self.output.flush()
             return res
         return func
