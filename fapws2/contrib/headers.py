e24f32e4868efc4e546b5856e2cd66da7b6e1266
william <william@opensource4you.com>
2007-11-17 17:59:49 +0100

    bug fix for http 302 reponse

--- fapws2/contrib/headers.py
+++ fapws2/contrib/headers.py
@@ -5,6 +5,6 @@ def redirect(start_response, location, permanent=None):
     if permanent:
         start_response('301 Moved Permanently', header)
     else:
-        start_response('302 Moved Permanently', header)
+        start_response('302 Moved Temporarily', header)
     return []
 
