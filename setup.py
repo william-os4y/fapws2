ff9e6025ad655e6fb3b6e46b0fedd43732feba9c
William.os4y <william.os4y@gmail.com>
2008-12-26 14:49:32 +0100

    Eggify the setup files (thanks to pjstevns)

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
-# copy these lines to a file named: setup.py
-# To build and install the elemlist module you need the following
-# setup.py file that uses the distutils:
-from distutils.core import setup, Extension
-import os
-import os.path 
-import sys
+from setuptools import setup, find_packages, Extension
+import sys, os
+
+version = '0.3'
+
 
 if os.environ.has_key('LIBEVENT_SRC'):
-        libevent_src = os.environ['LIBEVENT_SRC']
+    libevent_src = os.environ['LIBEVENT_SRC']
 else:
-        libevent_src = './libevent'
+    libevent_src = './libevent'
 
 if not os.path.exists('/usr/lib/libevent.so'):
     print "We don't find libevent installed!!!!"
@@ -18,26 +16,45 @@ print "We will use the followinf libevent sources:"
 print libevent_src
 print ""
 
-setup (name = "fapws2",
-       version = "0.1",
-       maintainer = "William",
-       maintainer_email = "william@opensource4you.com",
-       description = "Sample Python module",
-       packages= ['fapws2','fapws2/contrib'],
 
-       ext_modules = [
-	       Extension('_evhttp',
-	       sources=['fapws2/_evhttp.c'],
-           # I'm on an archlinux ;-)
-           # Here I'm pointing to the direcoty where libevent has been build
-           # In this directory wi can find sources and compiled objects (as after a "./configure; make")
-	       include_dirs=[libevent_src],
-	       library_dirs=["/usr/local/lib"], # add LD_RUN_PATH in your environment
-	       libraries=['event'],
-           #extra_compile_args=["-march=athlon-xp", "-mtune=athlon-xp", "-ggdb"],
-           #define_macros=[("DEBUG", "1")],
-	       )
-	       ]
-)
+def read_file(name):
+    return open(os.path.join(os.path.dirname(__file__),name)).read()
 
-# end of file: setup.py
+readme = read_file('README')
+
+setup(name='fapws2',
+      version=version,
+      description="fast asynchronous wsgi server",
+      long_description=readme,
+classifiers=['Development Status :: 4 - Beta','Environment :: Web Environment','License :: OSI Approved :: GNU General Public License (GPL)','Programming Language :: C','Programming Language :: Python','Topic :: Internet :: WWW/HTTP :: HTTP Servers','Topic :: Internet :: WWW/HTTP :: WSGI :: Server'], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='William',
+      author_email='william.os4y@gmail.com',
+      url='http://william-os4y.livejournal.com/',
+      license='GPL',
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+
+      packages= find_packages(),
+      ext_modules = [
+          Extension('_evhttp',
+                  sources=['fapws2/_evhttp.c'],
+                  # I'm on an archlinux ;-)
+                  # Here I'm pointing to the direcoty where libevent has been build
+                  # In this directory wi can find sources and compiled objects (as after a "./configure; make")
+                  include_dirs=[libevent_src],
+                  library_dirs=["/usr/local/lib"], # add LD_RUN_PATH in your environment
+                  libraries=['event'],
+                  #extra_compile_args=["-march=athlon-xp", "-mtune=athlon-xp", "-ggdb"],
+                  #define_macros=[("DEBUG", "1")],
+                  )
+                  ]
+
+
+      )
